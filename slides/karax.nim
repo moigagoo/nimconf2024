0a9b96470a4626f91251abd9dde47042352771b3
moigagoo <moigagoo@users.noreply.github.com>
2024-08-23 17:45:07 +0000

    deploy: 8854d6bf053bd10c1ce0d4d3964bd80116f01551

---
+++ slides/karax.nim
@@ -0,0 +1,21 @@
+import nimib, nimiSlides
+
+
+template karax* =
+  autoAnimateSlides(3):
+    nbText "## Karax" 
+    showAt(2):
+      nbText """
+-   Virtual DOM.
+-   VNode
+"""
+    showAt(3):
+      nbText """
+include karax/prelude
+
+proc render: VNode =
+  buildHtml:
+    tdiv:
+      h1: text "This is header"
+"""
+
