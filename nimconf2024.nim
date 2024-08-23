75d1abec711f38de798ea8b9b2617def9124d4df
moigagoo <moigagoo@users.noreply.github.com>
2024-08-23 14:19:00 +0000

    deploy: fa4dfea78d2ea1e993579fc2180c10e88ed76a8a

---
+++ nimconf2024.nim
@@ -0,0 +1,14 @@
+import nimib, nimiSlides
+
+import slides/[title, keypoints]
+
+
+nbInit(theme = revealTheme)
+
+setSlidesTheme(Dracula)
+
+title()
+keyPoints()
+
+nbSave()
+
---
+++ slides/keypoints.nim
@@ -0,0 +1,17 @@
+import nimib, nimiSlides
+
+
+template keyPoints* =
+  slide:
+    nbText """
+# Key Points
+
+1.  **Karax** is a great framework: fast, lightweight, stable.
+    -   But it lacks routing and components.
+
+1.  **Kraut** brings a routing layer.
+    -   **Sauer** makes adding routes trivial.
+
+1.  **Karkas** brings reusable layouts and components.
+"""
+
