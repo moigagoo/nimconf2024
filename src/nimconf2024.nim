99c408226acd50c32e392ac1ae8f76f84018aa59
Constantine Molchanov <moigagoo@duck.com>
2024-10-04 12:54:34 +0400

    Add preamble.

--- src/nimconf2024.nim
+++ src/nimconf2024.nim
@@ -1,13 +1,14 @@
 import nimib, nimiSlides
 
 import nimconf2024/utils
-import nimconf2024/slides/[title, keypoints, karax, kraut, sauer, karkas]
+import nimconf2024/slides/[title, preamble, keypoints, karax, kraut, sauer, karkas]
 
 
 nbInit(theme = revealTheme)
 
 slides:
   title
+  preamble
   keyPoints
   karax
   kraut
