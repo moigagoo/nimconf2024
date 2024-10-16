c40e252f42a3e2c0794d7cb56d6e0089d9e90767
moigagoo <moigagoo@users.noreply.github.com>
2024-10-16 10:45:30 +0000

    deploy: 8baeb3dfe229335f3006afe0dbbd62043bdb3752

--- nimconf2024/slides/kraut.nim
+++ nimconf2024/slides/kraut.nim
@@ -2,7 +2,7 @@ import nimib, nimiSlides
 
 
 template slide* =
-  autoAnimateSlides(3):
+  autoAnimateSlides(4):
     nbText "## Kraut" 
 
     showAt(1):
@@ -23,27 +23,44 @@ template slide* =
     showAt(3):
       nbText """
 ```nim
-include karax/prelude
-import kraut
-
-const
-  routes = {
-    "/": renderIndes,
-    "/users/": renderUsers,
-    "/users/{userId}/": renderUser
-  }
-
-setRenderer routeRenderer(routes)
+proc renderUser*(context: Context): VNode =
+  buildHtml:
+    tdiv:
+      p:
+        text "User id: " & context.urlParams["userId"]
 ```
 
 ```nim
 include karax/prelude
 import kraut
 
-proc renderUser*(context: Context): VNode =
-  buildHtml:
-    tdiv:
-      p:
-        text "User id: " & context.urlParams["userId"]
+
+const
+  routes = {
+    "/": renderIndex,
+    "/users/": renderUsers,
+    "/users/{userId}/": renderUser
+  }
+
+setRenderer routeRenderer(routes)
+"""
+    showAt(4):
+      nbImage("/img/kraut_app.png")
+
+    speakerNote """
+Most apps will have more than one location to render. Also, you will often need the same location show different data depending on the URL param. Vanilla Karax doesn't offer route management, and so this is how Kraut was created.
+
+Kraut implements URL parsing and matching. You can define routes as strings to be matched agaist the current URL.
+
+For every route, you define a proc to be executed when this route matches, very simple.
+
+Kraut builds upon Karax's built-in VNode type, meaning that your route rendering procs should just return a VNode.
+
+You can use variable parts in your routes and pass GET-params, Kraut will capture the values and pass them to the renderer.
+
+Here's our previous app but with routes. It renders essentially the same as the previous version but our code is much better now:
+1. Our routes are defined as a simple sequence of pairs. You can put your route definition into a separate module and even split it across several modules.
+2. Render proc can now live in a separate module as it receives all its context from Kraut.
+3. We no longer work with raw strings but instead have a nice table of params and their values.
 """
 
--- nimconf2024/slides/title.nim
+++ nimconf2024/slides/title.nim
@@ -8,4 +8,9 @@ template slide* =
 
 Create compact, fast, and composable frontend and have fun doing it 
 """
+    speakerNote """
+Hi! My name is Constantine, and I've been using Nim as my primary language for hobby development for over a decade.
+
+Today, I'd like to talk about applying Nim in frontend development. My goal is to show that Nim isn't just a viable option for frontend development (this is not news) but an option you can actually compare to the mainstream frameworks like React or Vue in terms of usability, reusability, app size, and programmer's experience.
+"""
 
